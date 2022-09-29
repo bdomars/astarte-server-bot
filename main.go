6941dd58cf36f7b77e08b6c4d53ac95746416bcd
Björn Domars <bdomars@walkbase.com>
2022-09-29 16:37:02 +0300

    playing around with keyboard and callback

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/kelseyhightower/envconfig"
 )
 
@@ -13,6 +13,11 @@ type BotConfig struct {
 	Token      string
 	WebhookUrl string
 }
+
+var serviceKeyboard = tgbotapi.NewInlineKeyboardMarkup([]tgbotapi.InlineKeyboardButton{
+	tgbotapi.NewInlineKeyboardButtonData("Satisfactory", "restart-satisfactory"),
+	tgbotapi.NewInlineKeyboardButtonData("Teamspeak", "restart-teamspeak"),
+})
 
 func main() {
 
@@ -32,19 +37,48 @@ func main() {
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	wh, _ := tgbotapi.NewWebhook(conf.WebhookUrl + bot.Token)
-
 	_, err = bot.Request(wh)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	updates := bot.ListenForWebhook("/" + bot.Token)
 	go http.ListenAndServe("0.0.0.0:8080", nil)
 
 	for update := range updates {
-		log.Printf("Got update: %+v\n", update)
-		receivedMessage := update.Message
-		if receivedMessage != nil {
-			log.Printf("Got message: %si from user %s\n", receivedMessage.Text, receivedMessage.From.UserName)
+
+		if update.Message != nil {
+			// Process a command
+			command := update.Message.Command()
+			switch command {
+			case "restart":
+				reply := tgbotapi.NewMessage(update.Message.Chat.ID, "Which service?")
+				reply.ReplyMarkup = serviceKeyboard
+				_, err := bot.Request(reply)
+				if err != nil {
+					log.Printf("reset message not set: %v", err.Error())
+				}
+			case "close":
+				msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
+				msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+				if _, err := bot.Send(msg); err != nil {
+					panic(err)
+				}
+			}
 		}
+
+		// Process a callback
+		if update.CallbackQuery != nil {
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			if _, err := bot.Request(callback); err != nil {
+				panic(err)
+			}
+
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Okay restarting...")
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
+		}
+
 	}
 }
